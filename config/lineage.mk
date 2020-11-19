ca8c5f6370a6a1ba7aa59d5f890e4de1486366cd
MSe1969 <mse1969@posteo.de>
2020-11-19 20:43:33 +0100

    Add RROs for Captive portal detection URLs
    By default, no RRO is activated - via static overlay in a previous commit,
    GrapheneOS is set as default (see vendor/linbeage/overlay)
    
    Change-Id: Ibef3f087e7c24a320aad3e547cdaba8b70e891fc

--- config/lineage.mk
+++ config/lineage.mk
@@ -1,3 +1,13 @@
 # Updater URI and changelog
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     lineage.updater.uri=https://raw.githubusercontent.com/LineageOS-Revived/ota_config/lineage-18.1/$(LINEAGE_BUILD).json
+
+#Captive-Portal detection overlays
+#(default: GrapheneOS - see vendor/lineage/overlay)
+PRODUCT_PACKAGES += \
+  overlay-204-android-Google \
+  overlay-204-android-openSUSE \
+  overlay-204-android-Ubuntu \
+  overlay-204-networkstack-Google \
+  overlay-204-networkstack-openSUSE \
+  overlay-204-networkstack-Ubuntu
