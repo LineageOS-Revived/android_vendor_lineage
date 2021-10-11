33cf68d48ac2b7ff980e3efea1f712562972cde2
Michael Bestas <mkbestas@lineageos.org>
2021-10-11 18:32:21 +0300

    config: Remove Email apps
    Change-Id: Iab9b6c38b4a118db57efb890c9069efe06632987

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -6,16 +6,11 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.notification_sound=Argon.ogg \
     ro.config.alarm_alert=Hassium.ogg
 
-# AOSP packages
-PRODUCT_PACKAGES += \
-    Email \
-    ExactCalculator \
-    Exchange2
-
-# Lineage packages
+# Apps
 PRODUCT_PACKAGES += \
     Backgrounds \
     Etar \
+    ExactCalculator \
     Jelly \
     Profiles \
     Seedvault
