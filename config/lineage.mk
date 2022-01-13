7f4cf73a23581a24aec00aa7b78546bec25bff77
Pranav Vashi <neobuddy89@gmail.com>
2022-01-13 17:07:39 +0530

    lineage: Build signal/wifi style overlays
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/lineage.mk
+++ config/lineage.mk
@@ -122,3 +122,22 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     SystemAmoledBlack \
     SystemCharcoalBlack
+
+# Signal / WiFi Icons
+PRODUCT_PACKAGES += \
+	BarsSignalOverlay \
+	BarsWiFiOverlay \
+	InsideSignalOverlay \
+	InsideWiFiOverlay \
+	RoundSignalOverlay \
+	RoundWiFiOverlay \
+	SneakySignalOverlay \
+	SneakyWiFiOverlay \
+	StrokeSignalOverlay \
+	StrokeWiFiOverlay \
+	WavySignalOverlay \
+	WavyWiFiOverlay \
+	XperiaSignalOverlay \
+	XperiaWiFiOverlay \
+	ZigZagSignalOverlay \
+	ZigZagWiFiOverlay
