f13d0dba69188c7fd2c9fd70ac5cfac724e6f9c3
SirRGB <sirrgb@proton.me>
2026-05-01 13:20:56 +0200

    Custom build version: 2026-05-01
    * https://review.lineageos.org/q/topic:%22R_asb_2026-05%22

--- config/lineage.mk
+++ config/lineage.mk
@@ -1,5 +1,5 @@
 # Unofficial patch level
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.lineage.custom_version=2026-04-01
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.lineage.custom_version=2026-05-01
 
 # Disable RescueParty due to high risk of data loss
 PRODUCT_PRODUCT_PROPERTIES += \
